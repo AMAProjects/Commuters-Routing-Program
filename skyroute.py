38d4901ce4b9bb7a200001d66199e7a1cd97bff2
Angela <angelamolinaalhambra@gmail.com>
2025-02-05 18:18:28 +0100

    Completed functions get_start_and_end, get_start and get_end

--- skyroute.py
+++ skyroute.py
@@ -20,6 +20,20 @@ def set_start_and_end(start_point, end_point): #This will handle setting the sel
   if start_point != None:
     change_point = input("What would you like to change? You can enter 'o' for 'origin', 'd' for 'destination', or 'b' for 'both': ")
     
+    if change_point == 'b':
+      start_point = get_start()
+      end_point = get_end()
+
+    elif change_point == 'o':
+      start_point = get_start()
+    
+    elif change_point == 'd':
+      end_point == get_end()
+    
+    else: #HANDLING USER INPUT ERRORS
+      print("Oops, that isn't 'o', 'd', or 'b'...")
+      set_start_and_end(start_point, end_point)
+
   else:
     start_point = get_start()
     end_point = get_end()
@@ -29,7 +43,28 @@ def set_start_and_end(start_point, end_point): #This will handle setting the sel
 
 
 def get_start(): # will be used to request an origin from the user.
-  pass
+  start_point_letter = input('"Where are you coming from? Type the corresponding letter: ')
+
+  if start_point_letter in landmark_choices:
+    start_point = landmark_choices[start_point_letter]
+  
+  else:
+    print("Sorry that's not a landmark we have data on. Let's try again...")
+    get_start()
+  
+  return start_point
 
 def get_end(): #to request and end from the user
-  pass
+  end_point_letter = input("Ok, where are you headed?: ")
+
+  if end_point_letter in landmark_choices:
+    end_point = landmark_choices[end_point_letter]
+
+  else:
+    print("Sorry that's not a landmark we have data on. Let's try again...")
+    get_end()
+  
+  return end_point
+
+
+print(set_start_and_end(None, None))
