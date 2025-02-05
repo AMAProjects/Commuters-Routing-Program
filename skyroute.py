f634b2591adfdd9c9150b800fcbb5fabc650eda7
Angela <angelamolinaalhambra@gmail.com>
2025-02-05 17:18:39 +0100

    Creating a variable that joins all of the landmarks together

--- skyroute.py
+++ skyroute.py
@@ -2,3 +2,8 @@ from graph_search import bfs, dfs
 from vc_metro import vc_metro
 from vc_landmarks import vc_landmarks
 from landmark_choices import landmark_choices
+
+#Start of building the program
+landmark_string = ''
+for letter, landmark in landmark_choices.items(): #.items() is used on a dictionary to access both the keys and values as pairs.
+  landmark_string += '{0} - {1}\n'.format(letter, landmark)
