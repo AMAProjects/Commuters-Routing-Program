7022ca08d0ac4e080e8930116090d31ed108abf3
Angela <angelamolinaalhambra@gmail.com>
2025-02-05 17:20:59 +0100

    Created greet and skyroute functions

--- skyroute.py
+++ skyroute.py
@@ -7,3 +7,11 @@ from landmark_choices import landmark_choices
 landmark_string = ''
 for letter, landmark in landmark_choices.items(): #.items() is used on a dictionary to access both the keys and values as pairs.
   landmark_string += '{0} - {1}\n'.format(letter, landmark)
+
+def greet():
+  print('Hi there and welcome to SkyRoute!')
+  print("We'll help you find the shortest route between the following Vancouver landmarks:\n" + landmark_string)
+
+def skyroute():
+  greet()
+
